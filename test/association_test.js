77d473f1620553a8627eba5b6f8257a5df0f25e7
Hal DeLage <hal.delage@teamaol.com>
2016-12-27 12:58:58 -0500

    Initial commit

---
+++ test/association_test.js
@@ -0,0 +1,54 @@
+const assert = require('assert');
+const User = require('../src/user');
+const Comment = require('../src/comment');
+const BlogPost = require('../src/blogPost');
+
+describe('Associations', () => {
+  let joe, blogPost, comment;
+  beforeEach((done) => {
+    joe = new User({name: 'Joe'});
+    blogPost = new BlogPost({title: 'JS is great!', content: 'Yup, it really is.'});
+    comment = new Comment({content: 'Congrats on great post.'});
+
+    joe.blogPosts.push(blogPost);
+    blogPost.comments.push(comment);
+    comment.user = joe;
+
+    Promise.all([
+      joe.save(),
+      blogPost.save(),
+      comment.save()
+    ]).then(() => done());
+  });
+
+  it('should save a relation between a user and a blogPost', (done) => {
+    User.findOne({name: 'Joe'})
+      .populate('blogPosts')
+      .then((user) => {
+        assert(user.blogPosts[0].title === 'JS is great!');
+        done();
+      });
+  });
+
+  it('should save a full relation graph', (done) => {
+    User.findOne({name: 'Joe'})
+      .populate({
+        path: 'blogPosts',
+        populate: {
+          path: 'comments',
+          model: 'comment',
+          populate: {
+            path: 'user',
+            model: 'user'
+          }
+        }
+      })
+      .then((user) => {
+        assert(user.name === 'Joe');
+        assert(user.blogPosts[0].title === 'JS is great!');
+        assert(user.blogPosts[0].comments[0].content === 'Congrats on great post.');
+        assert(user.blogPosts[0].comments[0].user.name === 'Joe');
+        done();
+      });
+  });
+});
